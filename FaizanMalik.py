24e426d1d9351ab0d200b4f7cdf131c6ee7e2db6
FaizanMalik302 <87928116+FaizanMalik302@users.noreply.github.com>
2021-07-25 14:22:49 +0500

    Create FaizanMalik.py
    https://github.com/FaizanMalik302/FaizanMalik302
    ls
    cd FaizanMalik302
    ls
    python2 FaizanMalik.py

---
+++ FaizanMalik.py
@@ -0,0 +1,18 @@
+### Hi Faizan Malik ðŸ‘‹
+
+
+
+
+
+
+- ðŸ”­ Iâ€™m currently working on Facebook
+- ðŸŒ± Iâ€™m currently learning Python
+- ðŸ‘¯ Iâ€™m looking to collaborate on Facebook
+- ðŸ¤” Iâ€™m looking for help with Documentary
+- ðŸ’¬ Ask me about Faizan Malik
+- ðŸ“« How to reach me: Facebook - @FaizanMalik
+- ðŸ˜„ Pronouns: He/His
+- âš¡ Fun fact: I Spend Almost Most Time To Use Facebook
+
+
+<img src="https://github-readme-stats.vercel.app/api?username=FaizanMalik302&&show_icons=true&title_color=ffffff&icon_color=bb2acf&text_color=daf7dc&bg_color=151515">
